3985b0c3162e5b98d75d23ad9ef0f88dbb1be42b
Yash Kankal <yashamolkankal@outlook.com>
2025-04-02 21:23:09 -0700

    new changes 2

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import Image from 'next/image';
 import { companies } from "@/data";
 
 const Clients = () => {
@@ -15,7 +16,13 @@ const Clients = () => {
           {companies.map((company) => (
             <React.Fragment key={company.id}>
               <div className="flex md:max-w-60 max-w-32 gap-2">
-                <img src={company.img} alt={company.name} className="md:w-10 w-5" />
+                <Image 
+                  src={company.img} 
+                  alt={company.name} 
+                  width={40}
+                  height={40}
+                  className="md:w-10 w-5" 
+                />
               </div>
             </React.Fragment>
           ))}
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 import { FaLocationArrow } from "react-icons/fa6";
+import Image from 'next/image';
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
 
@@ -6,16 +7,18 @@ const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
+        <Image
           src="/footer-grid.svg"
           alt="grid"
-          className="w-full h-full opacity-50 "
+          width={1920}
+          height={400}
+          className="w-full h-full opacity-50"
         />
       </div>
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
-          Let's have a<span className="text-purple"> conversation</span> 
+          Let&apos;s have a <span className="text-purple">conversation</span>
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
           Reach out to me today with the button below!
@@ -28,26 +31,30 @@ const Footer = () => {
           />
         </a>
       </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center w-full px-10">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Yash Amol Kankal 
+          Yash Amol Kankal
         </p>
         <div className="flex items-center md:gap-3 gap-6">
+          {/* GitHub */}
           <a
             href="https://github.com/yash-kankal"
             target="_blank"
             rel="noopener noreferrer"
             className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
           >
-            <img src={socialMedia[0].img} alt="GitHub" width={20} height={20} />
+            <Image src={socialMedia[0].img} alt="GitHub" width={20} height={20} />
           </a>
+
+          {/* LinkedIn */}
           <a
             href="https://www.linkedin.com/in/yashkankal/"
             target="_blank"
             rel="noopener noreferrer"
             className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
           >
-            <img src={socialMedia[1].img} alt="LinkedIn" width={20} height={20} />
+            <Image src={socialMedia[1].img} alt="LinkedIn" width={20} height={20} />
           </a>
         </div>
       </div>
